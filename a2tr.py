6986ee893cec4c8f865b86b3eb6615248ab5b816
Arell_Ge <2200013209@stu.pku.edu.cn>
2025-01-17 22:48:15 +0800

    Implement rethinking module

--- a2tr.py
+++ a2tr.py
@@ -1,4 +1,7 @@
 from utils import *
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("../all-mpnet-base-v2")
 
 file_path = "../clotho/clotho_captions_evaluation.csv"
 # audio_path = "../clotho/evaluation"
@@ -16,11 +19,18 @@ print(audio_captions[0])
 # 计算文本嵌入
 # text_embeddings = get_text_embedding_pengi(captions)
 # torch.save(text_embeddings, "text_embeddings.pt")
-text_embeddings = torch.load("text_embeddings.pt")
+# text_embeddings = torch.load("text_embeddings.pt")
+text_embeddings = torch.tensor(model.encode(captions))
 
 # audio_captions_embeddings = get_text_embedding_pengi(audio_captions)
 # torch.save(audio_captions_embeddings, "audio_captions_embeddings_beam5_size1.pt")
-audio_captions_embeddings = torch.load("audio_captions_embeddings_beam5_size1.pt")
+# audio_captions_embeddings = torch.load("audio_captions_embeddings_beam5_size1.pt")
+# audio_captions_embeddings = audio_captions_embeddings[0]
+audio_captions_embeddings = torch.tensor(model.encode(audio_captions))
 
 # 计算 Recall @ ks
-recall_atr_ks = compute_recall_at_k(audio_captions_embeddings, text_embeddings, ks)
+# recall_atr_ks = compute_recall_at_k(audio_captions_embeddings, text_embeddings, ks)
+RPG(audio_captions_embeddings, text_embeddings, ks, audio_captions, captions, batch_size=4, candidate_num=20)
+
+# SPIDEr = calculate_spider(hypotheses=audio_captions, references=captions)
+# print(SPIDEr)
